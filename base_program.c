5248a69193f2c018f94ffbd70528fa2c3f20343c
Raven <bloodraven166@gmail.com>
2021-09-08 08:11:21 -0700

    Create base_program.c

---
+++ base_program.c
@@ -0,0 +1,34 @@
+/* Program Information
+Program prompts user for two multiplication values and returns a product
+Program uses Four Step Programming Process
+*/
+
+// header files
+#include "Console_IO_Utility.h"
+
+// global constants
+const int TWO_ENDLINES = 2;
+const int BOX_WIDTH = 27;
+const int LEFT_PROMPT_WIDTH = 13;
+const int VALUE_WIDTH = 6;
+const int RIGHT_VALUE_WIDTH = 8;
+
+const char HEAVY_LINE = '=';
+const char THIN_LINE = '-';
+const char PIPE_SPACE[] = "| ";
+const char SPACE_PIPE[] = " |";
+
+// prototypes
+   // none
+
+// main program
+int main()
+   {
+
+       // return program success
+       return 0;
+   }
+
+
+// supporting functions
+   // none
